9689666d654bc6fa5dabda382def28a5a68ca9ca
Tom Bell <tomb@tombell.dev>
2025-03-11 22:25:46 +0000

    Add mod-tap for ctrl/esc

--- keyboards/crkbd/rev4_1/standard/keymaps/tombell/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/tombell/keymap.c
@@ -1,13 +1,13 @@
 #include QMK_KEYBOARD_H
 
-#define LCTLESC LCTL_T(KC_ESC)
+#define MT_LCTL LCTL_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3_ex2(
 //  ,--------------------------------------------------------------.                    ,--------------------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_HYPR,                      KC_HYPR,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
 //  |--------+--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      LCTLESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_MEH,                       KC_MEH,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      MT_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_MEH,                       KC_MEH,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
 //  |--------+--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                           KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
 //  |--------+--------+--------+--------+--------+--------'                                      `--------+--------+--------+--------+--------+--------'
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  ,--------------------------------------------------------------.                    ,--------------------------------------------------------------.
        KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_VOLU,                      KC_MUTE,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
 //  |--------+--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      LCTLESC,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_VOLD,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      MT_LCTL,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_VOLD,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 //  |--------+--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 //  |--------+--------+--------+--------+--------+--------'                                      `--------+--------+--------+--------+--------+--------'
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  ,--------------------------------------------------------------.                    ,--------------------------------------------------------------.
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_MSTP,                      KC_MNXT, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
 //  |--------+--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      LCTLESC,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_MPRV, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      MT_LCTL,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_MPRV, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
 //  |--------+--------+--------+-- -----+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                        KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
 //  |--------+--------+--------+--------+--------+--------'                                      `--------+--------+--------+--------+--------+--------'
